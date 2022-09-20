e452119a17d7a1251839d618e71d35b39bae28f0
Alexander Blaschuk <blaschuk95@gmail.com>
2022-09-20 14:52:36 +0300

    next-api

--- verification/referee.py
+++ verification/referee.py
@@ -1,14 +1,21 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
+from checkio.referees.io_template import CheckiOReferee
 
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
-    	tests=TESTS,
+        tests=TESTS,
         function_name={
             "python": "checkio",
             "js": "threeWords"
-        }).on_ready)
+        },
+        cover_code={
+            'python-3': {},
+            'js-node': {
+                # "dateForZeros": True,
+            }
+        }
+    ).on_ready)
